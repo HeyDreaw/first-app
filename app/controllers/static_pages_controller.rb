9b40bd2d4256a66b7c855fe4506a7de1be521c9b
HeyDreaw <asuppinimusic.com>
2016-03-18 13:57:37 -0700

    edited orders

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,10 @@ class StaticPagesController < ApplicationController
 		@featured_product = Product.first
 	end
 	
+	def landing_page
+		@products = Product.limit(3)
+	end
+	
   def index
   end
 end
