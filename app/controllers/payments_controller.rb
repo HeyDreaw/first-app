2f87efeff2ab0862816dbb54857f8508bf863f67
HeyDreaw <asuppinimusic@gmail.com>
2016-06-29 15:01:02 -0700

    added payment controller

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,23 @@
+class PaymentsController < ApplicationController
+	def create
+    @product = params[:product_id]
+    @user = current_user
+    token = params[:stripeToken]
+    begin
+      charge = Stripe::Charge.create(
+      :amount => @product.price,
+      :currency => "usd",
+      :source => token,
+      :description => params[:stripeEmail]
+      )
+      if charge.paid
+        Order.create(product_id: @product.id, user_id: @user, total: @product.price)
+      end
+    rescue Stripe::CardError => e
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    end
+    redirect_to product_path(@product)
+  end
+end
