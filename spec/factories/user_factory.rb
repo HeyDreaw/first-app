34ecdb848f203aa6e513b61d6fdec54d5918ba13
HeyDreaw <asuppinimusic@gmail.com>
2016-06-29 15:53:23 -0700

    fixed user controller

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -7,12 +7,14 @@ FactoryGirl.define do
     last_name "Example"
     admin false
   end
+	
+	factory :admin, class: User do
+		email 
+		password "7654321"
+		admin true
+		first_name "Admin"
+		last_name "User"
+	end
+	
 end
 
-factory :admin, class: User do
-  email 
-  password "7654321"
-  admin true
-  first_name "Admin"
-  last_name "User"
-end
