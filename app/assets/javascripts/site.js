d47965c7679532f746842bb979845526c45fa022
HeyDreaw <asuppinimusic@gmail.com>
2016-06-29 20:07:56 -0700

    added jquery zoom plugin

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -6,4 +6,13 @@ $(document).on('ready page:load', function(){
         return $(this).attr('data-score');
       }
     });
+	
+$('.img-zoom').elevateZoom({
+    		constrainType:"height",
+    		constrainSize:274,
+    		zoomType: "lens",
+    		containLensZoom: true,
+    		scrollZoom: true,
+  	});
+	
 });
