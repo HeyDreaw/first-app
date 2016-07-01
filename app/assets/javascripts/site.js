8e4e6985ddf44d688120e314084a559cd46baff8
HeyDreaw <asuppinimusic@gmail.com>
2016-06-30 17:24:22 -0700

    fixed jquery zoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -8,11 +8,8 @@ $(document).on('ready page:load', function(){
     });
 	
 $('.img-zoom').elevateZoom({
-    		constrainType:"height",
-    		constrainSize:274,
-    		zoomType: "lens",
-    		containLensZoom: true,
-    		scrollZoom: true,
-  	});
+ 		zoomType	: "inner", cursor: "crosshair"
+ 
+ 	});
 	
 });
